3f541e9d64be179d15522b7e1045e3814fdc73ac
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-31 10:01:40 -0500

    added auth information to post to page upon login

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -11,7 +11,15 @@ export default class Facebook extends Component {
   };
 
   responseFacebook = response => {
-    console.log(response);
+    // console.log(response);
+
+    this.setState({
+      isLoggedIn: true,
+      userID: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url
+    });
   };
 
   componentClicked = () => console.log("clicked");
@@ -20,7 +28,20 @@ export default class Facebook extends Component {
     let fbContent;
 
     if (this.state.isLoggedIn) {
-      fbContent = null;
+      fbContent = (
+        <div
+          style={{
+            width: "400px",
+            margin: "auto",
+            background: "#f4f4f4",
+            padding: "20px"
+          }}
+        >
+          <img src={this.state.picture} alt={this.state.name} />
+          <h2>Welcome {this.state.name}</h2>
+          Email: {this.state.email}
+        </div>
+      );
     } else {
       fbContent = (
         <FacebookLogin
