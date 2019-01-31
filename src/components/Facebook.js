5a86e6a230402621ab2ceb68659f96c89a945012
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-01-31 09:44:53 -0500

    added Facebook.js

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
 
 export default class Facebook extends Component {
+  state = {
+    isloggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: ""
+  };
+
+  responseFacebook = response => {
+    console.log(response);
+  };
+
+  componentClicked = () => console.log("clicked");
+
   render() {
-    return <div />;
+    let fbContent;
+
+    if (this.state.isLoggedIn) {
+      fbContent = null;
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId="390313968399887"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
+    }
+
+    return <div>{fbContent}</div>;
   }
 }
